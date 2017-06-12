0a9714d9da948d2760076f27a937197b06fcfdd5
chenh <feihong729@163.com>
2017-06-12 18:28:39 +0800

    评论

---
+++ ALPersonalDemo/ALPersonalDemo/ALTool/UIKit/评论组件/Model/MHYouKuItem.h
@@ -0,0 +1,16 @@
+//
+//  MHYouKuItem.h
+//  MHDevelopExample
+//
+//  Created by CoderMikeHe on 17/2/17.
+//  Copyright © 2017年 CoderMikeHe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MHYouKuItem : NSObject
+
+/** title */
+@property (nonatomic , copy) NSString * title;
+
+@end
--- ALPersonalDemo/ALPersonalDemo/ViewController/ALDemo1ViewController.m
+++ ALPersonalDemo/ALPersonalDemo/ViewController/ALDemo1ViewController.m
@@ -11,20 +11,28 @@
 
 @interface ALDemo1ViewController ()
 @property(nonatomic,strong)WWTagEditingView *edtingView;
+
+@property (nonatomic,strong) UITextView *textView;
+
 @end
 
 @implementation ALDemo1ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+}
+
+-(void)testTEXTKit{
     self.edtingView = [[WWTagEditingView alloc]initWithFrame:CGRectMake(10, 80, self.view.bounds.size.width-20, 200)];
     self.edtingView.backgroundColor = [UIColor grayColor];
     [self.view addSubview:self.edtingView];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    self.textView = [[UITextView alloc] initWithFrame:CGRectMake(kALScreenWidth / 4, kView_BOTTOMY(self.edtingView), kALScreenWidth / 2, 100)];
+    self.textView.text = @"hellllll";
+    self.textView.editable = NO;
+    self.textView.scrollEnabled = NO;
+    [self.view addSubview:self.textView];
 }
 
 
