4e42db3461b4ea839c0d1b7ce52882c06ebead08
chenh <feihong729@163.com>
2017-12-13 16:47:40 +0800

    添加文案

--- ALPersonalDemo/ALPersonalDemo/ALTool/第三方/SNFoundation/SNFoundation/SNDefines.h
+++ ALPersonalDemo/ALPersonalDemo/ALTool/第三方/SNFoundation/SNFoundation/SNDefines.h
@@ -122,5 +122,82 @@ _Pragma("clang diagnostic pop") \
 #define ICTextViewLog(...)
 #endif
 
+#define BAKit_Objc_setObj(key, value) objc_setAssociatedObject(self, key, value, OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+
+#define BAKit_Objc_setObjCOPY(key, value) objc_setAssociatedObject(self, key, value, OBJC_ASSOCIATION_COPY)
+
+#define BAKit_Objc_getObj objc_getAssociatedObject(self, _cmd)
+
+#define BAKit_Objc_exchangeMethodAToB(methodA,methodB) method_exchangeImplementations(class_getInstanceMethod([self class], methodA),class_getInstanceMethod([self class], methodB));
+
+#pragma mark - NotiCenter
+#define BAKit_NotiCenter [NSNotificationCenter defaultCenter]
+
+/*!
+ *  获取屏幕宽度和高度
+ */
+#define BAKit_SCREEN_WIDTH ((([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait) || ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortraitUpsideDown)) ? [[UIScreen mainScreen] bounds].size.width : [[UIScreen mainScreen] bounds].size.height)
+
+#define BAKit_SCREEN_HEIGHT ((([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait) || ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortraitUpsideDown)) ? [[UIScreen mainScreen] bounds].size.height : [[UIScreen mainScreen] bounds].size.width)
+
+#pragma mark - weak / strong
+#define BAKit_WeakSelf        @BAKit_Weakify(self);
+#define BAKit_StrongSelf      @BAKit_Strongify(self);
+
+/*！
+ * 强弱引用转换，用于解决代码块（block）与强引用self之间的循环引用问题
+ * 调用方式: `@BAKit_Weakify`实现弱引用转换，`@BAKit_Strongify`实现强引用转换
+ *
+ * 示例：
+ * @BAKit_Weakify
+ * [obj block:^{
+ * @strongify_self
+ * self.property = something;
+ * }];
+ */
+#ifndef BAKit_Weakify
+#if DEBUG
+#if __has_feature(objc_arc)
+#define BAKit_Weakify(object) autoreleasepool{} __weak __typeof__(object) weak##_##object = object;
+#else
+#define BAKit_Weakify(object) autoreleasepool{} __block __typeof__(object) block##_##object = object;
+#endif
+#else
+#if __has_feature(objc_arc)
+#define BAKit_Weakify(object) try{} @finally{} {} __weak __typeof__(object) weak##_##object = object;
+#else
+#define BAKit_Weakify(object) try{} @finally{} {} __block __typeof__(object) block##_##object = object;
+#endif
+#endif
+#endif
+
+/*！
+ * 强弱引用转换，用于解决代码块（block）与强引用对象之间的循环引用问题
+ * 调用方式: `@BAKit_Weakify(object)`实现弱引用转换，`@BAKit_Strongify(object)`实现强引用转换
+ *
+ * 示例：
+ * @BAKit_Weakify(object)
+ * [obj block:^{
+ * @BAKit_Strongify(object)
+ * strong_object = something;
+ * }];
+ */
+#ifndef BAKit_Strongify
+#if DEBUG
+#if __has_feature(objc_arc)
+#define BAKit_Strongify(object) autoreleasepool{} __typeof__(object) object = weak##_##object;
+#else
+#define BAKit_Strongify(object) autoreleasepool{} __typeof__(object) object = block##_##object;
+#endif
+#else
+#if __has_feature(objc_arc)
+#define BAKit_Strongify(object) try{} @finally{} __typeof__(object) object = weak##_##object;
+#else
+#define BAKit_Strongify(object) try{} @finally{} __typeof__(object) object = block##_##object;
+#endif
+#endif
+#endif
+
+
 
 #endif
