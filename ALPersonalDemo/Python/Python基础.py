9496d728795b747a622945c98950abab68c20023
chenh <feihong729@163.com>
2018-02-03 16:17:44 +0800

    python文案、加入

--- ALPersonalDemo/Python/Python基础.py
+++ ALPersonalDemo/Python/Python基础.py
@@ -383,6 +383,281 @@ deepcopy()函数将同时复制它们内部的列表
 
 
 5.1 字典数据类型
+>>> myCat = {'size': 'fat', 'color': 'gray', 'disposition': 'loud'}
+>>> myCat['size']
+字典仍然可以用整数值作为键， 就像列表使用整数值作为下标一样， 但它们不必从 0 开始，可以是任何数字。
+>>> spam = {12345: 'Luggage Combination', 42: 'The Answer'}
+
+
+5.1.1 字典与列表
+>>> spam = ['cats', 'dogs', 'moose']
+>>> bacon = ['dogs', 'moose', 'cats']
+>>> spam == bacon
+False
+>>> eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+>>> ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+>>> eggs == ham
+True
+
+
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+while True:
+    print('Enter a name: (blank to quit)')
+    name = input()
+    if name == '':
+       break
+    if name in birthdays:
+       print(birthdays[name] + ' is the birthday of ' + name)
+    else:
+       print('I do not have birthday information for ' + name)
+       print('What is their birthday?')
+       bday = input()
+       birthdays[name] = bday
+       print('Birthday database updated.')
+
+
+5.1.2 keys()、 values()和 items()方法
+
+spam = {'color': 'red', 'age': 42}
+for v in spam.values():
+    print(v)
+for k in spam.keys():
+    print(k)
+for i in spam.items():
+    print(i)
+#如果希望通过这些方法得到一个真正的列表，就把类似列表的返回值传递给 list
+函数。  
+>>> spam.keys()
+dict_keys(['color', 'age'])
+>>> list(spam.keys())
+['color', 'age']
+
+>>> for k, v in spam.items():
+    print('Key: ' + k + ' Value: ' + str(v))
+Key: color Value: red
+Key: age Value: 42
+
+
+5.1.3 检查字典中是否存在键或值
+in 和 not in 操作符可以检查值是否存在于列表中。也可以利用这些操作符，检查某个键或值是否存在于字典中。
+
+>>> 'color' not in spam.keys()
+True
+>>> 'color' in spam
+False
+'color' in spam 本质上是一个简写版本。相当于'color' in spam.keys()。
+
+
+5.1.4 get()方法
+它有两个参数：要取得其值的键，以及如果该键不存在时，返回的备用值。
+>>> picnicItems = {'apples': 5, 'cups': 2}
+>>> 'I am bringing ' + str(picnicItems.get('cups', 0)) + ' cups.'
+'I am bringing 2 cups.'
+>>> 'I am bringing ' + str(picnicItems.get('eggs', 0)) + ' eggs.'
+'I am bringing 0 eggs.'
+
+
+5.1.5 setdefault()方法
+>>> spam = {'name': 'Pooka', 'age': 5}
+>>> spam.setdefault('color', 'black')
+'black'
+>>> spam
+{'color': 'black', 'age': 5, 'name': 'Pooka'}
+>>> spam.setdefault('color', 'white')
+'black'
+>>> spam
+{'color': 'black', 'age': 5, 'name': 'Pooka'}
+
+程序循环迭代 message 字符串中的每个字符，计算每个字符出现的次数。 
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+print(count)
+
+
+5.2 漂亮打印
+import pprint
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+pprint.pprint(count)
+
+
+5.3.2 嵌套的字典和列表
+allGuests = {'Alice': {'apples': 5, 'pretzels': 12},
+'Bob': {'ham sandwiches': 3, 'apples': 2},
+'Carol': {'cups': 3, 'apple pies': 1}}
+def totalBrought(guests, item):
+    numBrought = 0
+    for k, v in guests.items():
+        numBrought = numBrought + v.get(item, 0)
+    return numBrought
+print('Number of things being brought:')
+print(' - Apples ' + str(totalBrought(allGuests, 'apples')))
+print(' - Cups ' + str(totalBrought(allGuests, 'cups')))
+print(' - Cakes ' + str(totalBrought(allGuests, 'cakes')))
+print(' - Ham Sandwiches ' + str(totalBrought(allGuests, 'ham sandwiches')))
+print(' - Apple Pies ' + str(totalBrought(allGuests, 'apple pies')))
+
+
+第6章
+6.1 处理字符串
+6.1.2 双引号
+>>> spam = "That is Alice's cat."
+6.1.3 转义字符
+>>> spam = 'Say hi to Bob\'s mother.'
+6.1.4 原始字符串
+可以在字符串开始的引号之前加上 r， 使它成为原始字符串。“ 原始字符串” 完全忽略所有的转义字符， 打印出字符串中所有的倒斜杠。
+6.1.5 用三重引号的多行字符串
+print('''Dear Alice,
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+Sincerely,
+Bob''')
+6.1.6 多行注释
+"""This is a test Python program.
+Written by Al Sweigart al@inventwithpython.com
+This program was designed for Python 3, not Python 2.
+"""
+def spam():
+    """This is a multiline comment to help
+    explain what the spam() function does."""
+    print('Hello!')
+spam()
+
+
+6.1.7 字符串下标和切片
+6.1.8 字符串的 in 和 not in 操作符
+
+6.2.1 字符串方法 upper()、 lower()、 isupper()和 islower()
+如果字符串至少有一个字母， 并且所有字母都是大写或小写， isupper()和islower()方法就会相应地返回布尔值 True。否则， 该方法返回 False。
+>>> spam = 'Hello world!'
+>>> spam.islower()
+False
+>>> spam.isupper()
+False
+>>> 'HELLO'.isupper()
+True
+
+
+6.2.2 isX 字符串方法
+ isalpha()返回 True， 如果字符串只包含字母， 并且非空；
+ isalnum()返回 True，如果字符串只包含字母和数字，并且非空；
+ isdecimal()返回 True，如果字符串只包含数字字符，并且非空；
+ isspace()返回 True，如果字符串只包含空格、制表符和换行，并且非空；
+ istitle()返回 True，如果字符串仅包含以大写字母开头、后面都是小写字母的单词。
+
+>>> 'This Is not Title Case'.istitle()
+False
+
+
+6.2.3 字符串方法 startswith()和 endswith()
+startswith()和 endswith()方法返回 True， 如果它们所调用的字符串以该方法传入的字符串开始或结束。否则， 方法返回 False。
+
+>>> 'Hello world!'.startswith('Hello')
+True
+
+6.2.4 字符串方法 join()和 split()
+>>> ', '.join(['cats', 'rats', 'bats'])
+'cats, rats, bats'
+>>> ' '.join(['My', 'name', 'is', 'Simon'])
+'My name is Simon'
+>>> 'ABC'.join(['My', 'name', 'is', 'Simon'])
+'MyABCnameABCisABCSimon'
+
+>>> 'My name is Simon'.split()
+['My', 'name', 'is', 'Simon']
+默认情况下，字符串'My name is Simon'按照各种空白字符分割， 诸如空格、 制表符或换行符。这些空白字符不包含在返回列表的字符串中。
+>>> 'MyABCnameABCisABCSimon'.split('ABC')
+['My', 'name', 'is', 'Simon']
+
+6.2.5 用 rjust()、 ljust()和 center()方法对齐文本
+rjust()和 ljust()字符串方法返回调用它们的字符串的填充版本， 通过插入空格来对齐文本。 这两个方法的第一个参数是一个整数长度， 用于对齐字符串。  
+>>> 'Hello World'.rjust(20)
+'         Hello World'
+>>> 'Hello'.ljust(10)
+'Hello     '
+
+>>> 'Hello'.rjust(20, '*')
+'***************Hello'
+
+>>> 'Hello'.center(20)
+'       Hello        '
+>>> 'Hello'.center(20, '=')
+'=======Hello========'
+
+def printPicnic(itemsDict, leftWidth, rightWidth):
+    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
+    for k, v in itemsDict.items():
+        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
+printPicnic(picnicItems, 12, 5)
+printPicnic(picnicItems, 20, 6)
+
+
+6.2.6 用 strip()、 rstrip()和 lstrip()删除空白字符
+strip()字符串方法将返回一个新的字符串， 它的开头或末尾都没有空白字符。
+lstrip()和 rstrip()方法将相应删除左边或右边的空白字符。
+
+>>> spam = ' Hello World '
+>>> spam.strip()
+'Hello World'
+>>> spam.lstrip()
+'Hello World '
+>>> spam.rstrip()
+' Hello World'
+>>> 
+
+向 strip()方法传入参数'ampS'， 告诉它在变量中存储的字符串两端， 删除出现的
+a、 m、 p 和大写的 S。传入 strip()方法的字符串中， 字符的顺序并不重要
+
+
+6.2.7 用 pyperclip 模块拷贝粘贴字符串
+>>> import pyperclip
+>>> pyperclip.copy('Hello world!')
+>>> pyperclip.paste()
+'Hello world!
+
+
+6.3 项目：口令保管箱
+#! python3
+# pw.py - An insecure password locker program.
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+'luggage': '12345'}
+
+import sys
+if len(sys.argv) < 2:
+   print('Usage: python pw.py [account] - copy account password')
+   sys.exit()
+account = sys.argv[1] # first command line arg is the account name - sys.argv 列表中的第一项总是一个字符串，它包含程序的文件名（ 'pw.py'）。第二项应该是第一个命令行参数。
+
+if account in PASSWORDS:
+   pyperclip.copy(PASSWORDS[account])
+   print('Password for ' + account + ' copied to clipboard.')
+else:
+   print('There is no account named ' + account)
+
+
+6.4 项目： 在 Wiki 标记中添加无序列表
+#! python3
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard.
+import pyperclip
+text = pyperclip.paste()
+# Separate lines and add stars.
+lines = text.split('\n')
+for i in range(len(lines)): # loop through all indexes for "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
+text = '\n'.join(lines)
+pyperclip.copy(text)
+
+
+
 
 
 
